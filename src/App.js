5f5c020194e7d1284e7dd8e8ae0c0f46e72f0e22
dzdanowicz <contact@dzdanowicz.com>
2022-08-08 02:18:40 +0200

    Add Font Awesome and App component with basic structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquareTwitter } from "@fortawesome/free-brands-svg-icons";
+import ReactDOM from "react-dom";
 import "./App.css";
 
-function App() {
-  return ()
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quoteIndex: undefined,
+    };
+  }
+
+  render() {
+    return (
+      <div id="quote-box">
+        <p id="text"></p>
+        <p id="author"></p>
+        <div id="buttons">
+          <a href="https://twitter.com">
+            <FontAwesomeIcon icon={faSquareTwitter} />
+          </a>
+          <button id="new-quote"></button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
