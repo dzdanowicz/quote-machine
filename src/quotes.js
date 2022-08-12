690d4f8da6114942dbbd050bf1429be8047b8fdb
dzdanowicz <contact@dzdanowicz.com>
2022-08-12 14:57:33 +0200

    Add quotes.js file

---
+++ src/quotes.js
@@ -0,0 +1,36 @@
+const quotes = [
+  {
+    text: "Be yourself; everyone else is already taken.",
+    author: "Oscar Wilde",
+  },
+  {
+    text: "Failure will never overtake me if my determination to succeed is strong enough.",
+    author: "Og Mandino",
+  },
+  {
+    text: "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
+    author: "Benjamin Franklin",
+  },
+  {
+    text: "I've missed more than 9,000 shots during my career. I've lost almost 300 games. 26 times, I've been trusted to take the game winning shot and missed. I've failed over and over and over again in my life. And that is why I succeed.",
+    author: "Michael Jordan",
+  },
+  {
+    text: "Those who can imagine anything, can create the impossible.",
+    author: "Alan Turing",
+  },
+  {
+    text: "It's fine to celebrate success, but it is more important to heed the lessons of failure.",
+    author: "Bill Gates",
+  },
+  {
+    text: "You can never cross the ocean unless you have the courage to lose sight of the shore.",
+    author: "Christopher Columbus",
+  },
+  {
+    text: "It had long since come to my attention that people of accomplishment rarely sat back and let things happen to them. They went out and happened to things.",
+    author: "Leonardo da Vinci",
+  },
+];
+
+export default quotes;
