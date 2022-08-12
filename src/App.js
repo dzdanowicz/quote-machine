28b0ff2268aefe80ff44d2cb5c21e09bbde8ab29
dzdanowicz <contact@dzdanowicz.com>
2022-08-12 18:28:16 +0200

    Create getRandomQuote method that returns random quote

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import quotes from "./quotes";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSquareTwitter } from "@fortawesome/free-brands-svg-icons";
-import ReactDOM from "react-dom";
 import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    const randomNum = Math.floor(Math.random() * quotes.length);
     this.state = {
-      quoteIndex: undefined,
+      text: quotes[randomNum].text,
+      quoteIndex: randomNum,
     };
+    this.handleClick = this.handleClick.bind(this);
+    this.getRandomQuote = this.getRandomQuote.bind(this);
   }
+
+  getRandomQuote() {
+    let randomNum;
+    while (randomNum === this.state.quoteIndex) {
+      randomNum = Math.floor(Math.random() * quotes.length);
+    }
+    return quotes[randomNum];
+  }
+
+  handleClick() {}
 
   render() {
     return (
       <div id="quote-box">
-        <p id="text"></p>
+        <p id="text">{this.state.text}</p>
         <p id="author"></p>
         <div id="buttons">
           <a href="https://twitter.com">
             <FontAwesomeIcon icon={faSquareTwitter} />
           </a>
-          <button id="new-quote"></button>
+          <button id="new-quote" onClick={this.handleClick}>
+            New Quote
+          </button>
         </div>
       </div>
     );
